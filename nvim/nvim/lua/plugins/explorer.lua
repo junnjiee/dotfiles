3e19113936f09043fb70961d66f12f9ab4721568
jj <71215065+junnjiee@users.noreply.github.com>
2026-05-05 14:56:19 +0800

    nvim config

--- nvim/nvim/lua/plugins/explorer.lua
+++ nvim/nvim/lua/plugins/explorer.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    "folke/snacks.nvim",
-    opts = {
-      picker = {
-        sources = {
-          explorer = {
-            layout = { hidden = { "input" } },
-            hidden = true, -- Show hidden files by default
-            ignored = true, -- Show gitignored files by default (this includes .env)
-          },
-        },
-      },
-    },
-  },
+	{
+		"folke/snacks.nvim",
+		opts = {
+			picker = {
+				sources = {
+					explorer = {
+						layout = { hidden = { "input" } },
+						hidden = true, -- hidden files
+						ignored = false, -- gitignored files
+					},
+				},
+			},
+		},
+	},
 }
--- nvim/nvim/lua/plugins/tau.lua
+++ nvim/nvim/lua/plugins/tau.lua
@@ -1,16 +1,16 @@
 return {
-	{
-		"timothyckl/tau.nvim",
-		build = "cd cli && bun build --compile src/index.ts --outfile tau",
-		config = function()
-			require("tau").setup({
-				api_url = "https://api.openai.com/v1",
-				api_key = vim.fn.getenv("OPENAI_API_KEY"),
-				model = "gpt-5.4",
-			})
-		end,
-		keys = {
-			{ "<leader>t", ":Tau ", mode = "v", desc = "Tau: LLM edit selection" },
-		},
+	"timothyckl/tau.nvim",
+	lazy = false,
+	build = "cd cli && bun run build",
+	config = function()
+		require("tau").setup({
+			api_url = "https://api.openai.com/v1",
+			api_key = vim.fn.getenv("OPENAI_API_KEY"),
+			model = "gpt-5.4",
+		})
+	end,
+	keys = {
+		{ "<leader>t", ":Tau<CR>", mode = "v", desc = "Tau: edit selection" },
+		{ "<C-t>", ":TauContext<CR>", mode = "n", desc = "Tau: context files" },
 	},
 }
